1aa09fe383f3aded8cecd39ea9b3aac8c431ebee
Julian Swerdlin <julianswer36@gmail.com>
2021-12-18 00:31:09 +0100

    backend files

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,7 +1,23 @@
 import { Router } from 'express'
 const router = Router()
 
-router.get('/', (req, res) => {
+router.get('/tasks', (req, res) => {
+    res.send('Hello world');
+})
+
+router.post('/tasks', (req, res) => {
+    res.send('Hello world');
+})
+
+router.get('/tasks/:id', (req, res) => {
+    res.send('Hello world');
+})
+
+router.delete('/tasks/:id', (req, res) => {
+    res.send('Hello world');
+})
+
+router.put('/tasks/:id', (req, res) => {
     res.send('Hello world');
 })
 
