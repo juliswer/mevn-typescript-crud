e60f21dc825a53e810059f0a34e0ea5a5fa7abc1
Julian Swerdlin <julianswer36@gmail.com>
2021-12-19 02:18:39 +0100

    backend files

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,31 +1,39 @@
 <template>
-    <ul>
-        <li v-for="(task, index) in tasks" :key="index" @click="this.$router.push(`/tasks/${task._id}`)">
-            {{task.title}}
+  <div class="container p-5">
+      <ul class="list-group">
+        <li
+          class="list-group-item list-group-item-action"
+          style="cursor: pointer"
+          v-for="(task, index) in tasks"
+          :key="index"
+          @click="this.$router.push(`/tasks/${task._id}`)"
+        >
+        {{index + 1}}.
+          {{ task.title }}
         </li>
     </ul>
+  </div>
 </template>
 
 <script lang="ts">
-import { Task } from '@/interfaces/Task'
-import { getTasks } from '@/services/TaskService'
-import { defineComponent} from 'vue'
-
+import { Task } from "@/interfaces/Task";
+import { getTasks } from "@/services/TaskService";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    data() {
-        return {
-            tasks: [] as Task[]
-        }
+  data() {
+    return {
+      tasks: [] as Task[],
+    };
+  },
+  methods: {
+    async loadTasks() {
+      const res = await getTasks();
+      this.tasks = res.data;
     },
-    methods: {
-        async loadTasks() {
-            const res = await getTasks()
-            this.tasks = res.data
-        }
-    },
-    mounted() {
-        this.loadTasks()
-    }
-})
+  },
+  mounted() {
+    this.loadTasks();
+  },
+});
 </script>
