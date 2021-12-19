261969b99c7675bfc202ec66b117fb20b7b6b5c5
Julian Swerdlin <julianswer36@gmail.com>
2021-12-19 02:27:23 +0100

    backend files

--- src/components/TaskDetail.vue
+++ src/components/TaskDetail.vue
@@ -1,15 +1,17 @@
 <template>
-    <h1>Task Detail</h1>
+    <div class="col-md-4 offset-md-4">
 
-    <form @submit.prevent="handleUpdate()">
-        <input type="text" v-model="currentTask.title">
+            <form @submit.prevent="handleUpdate()" class="card card-body">
+                <h1 class="text-center h3 mb-3">Actualiza o elimina {{currentTask.title}}</h1>
+                <input type="text" v-model="currentTask.title" class="form-control mb-3">
 
-        <textarea rows="3" v-model="currentTask.description"></textarea>
+                <textarea rows="3" v-model="currentTask.description" class="form-control mb-3"></textarea>
 
-        <button>update</button>
-    </form>
+                <button class="btn btn-primary">Update</button>
+            </form>
 
-    <button @click="handleDelete()">Delete</button>
+            <button @click="handleDelete()" class="btn btn-danger">Delete</button>
+    </div>
 
 </template>
 
--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -4,7 +4,7 @@
 
             <h1 class="text-center h3 mb-3">Create Task</h1>
 
-            <input type="text" placeholder="Write a title" v-model="task.title" class="form-control mb-3">
+            <input type="text" autofocus placeholder="Write a title" v-model="task.title" class="form-control mb-3">
 
             <textarea rows="3" placeholder="Write a description" v-model="task.description" class="form-control mb-3"></textarea>
 
