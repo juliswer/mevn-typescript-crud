53bf492d81f1971cac979d73cfc36ed8acb210ae
Julian Swerdlin <julianswer36@gmail.com>
2021-12-19 01:43:34 +0100

    backend files

--- src/components/TaskDetail.vue
+++ src/components/TaskDetail.vue
@@ -1,3 +1,14 @@
 <template>
     <h1>Task Detail</h1>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core"
+
+export default defineComponent({
+    name: "TaskDetail",
+    mounted() {
+        console.log(this.$route.params.id);
+    }
+})
+</script>
