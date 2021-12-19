1d7e741b02d40f7a2dac289100f34a55c5eadedf
Julian Swerdlin <julianswer36@gmail.com>
2021-12-19 01:52:34 +0100

    backend files

--- src/components/TaskDetail.vue
+++ src/components/TaskDetail.vue
@@ -1,17 +1,30 @@
 <template>
     <h1>Task Detail</h1>
+
+    <form>
+        <input type="text" v-model="currentTask.title">
+
+        <textarea rows="3" v-model="currentTask.description"></textarea>
+    </form>
+
 </template>
 
 <script lang="ts">
+import { Task } from "@/interfaces/Task";
 import { getTask } from "@/services/TaskService";
 import { defineComponent } from "@vue/runtime-core"
 
 export default defineComponent({
     name: "TaskDetail",
+    data() {
+        return {
+            currentTask: {} as Task
+        }
+    },
     methods: {
         async loadTask(id : string) {
             const res = await getTask(id)
-            console.log(res);
+            this.currentTask = res.data
         }
     },
     mounted() {
