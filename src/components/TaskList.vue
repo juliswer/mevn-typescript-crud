e7f925245616a84fed15e5681c2879152417506c
Julian Swerdlin <julianswer36@gmail.com>
2021-12-19 01:18:26 +0100

    backend files

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,3 +1,21 @@
 <template>
     <h1>Task list</h1>
 </template>
+
+<script lang="ts">
+import { getTask } from '@/services/TaskService'
+import { defineComponent} from 'vue'
+
+
+export default defineComponent({
+    methods: {
+        async loadTasks() {
+            const res = await getTask()
+            console.log(res);
+        }
+    },
+    mounted() {
+        this.loadTasks()
+    }
+})
+</script>
--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -1,4 +1,6 @@
 import axios from './axios'
 import {Task} from '@/interfaces/Task'
 
+export const getTask = async () => await axios.get('/tasks')
+
 export const createTask = async (task: Task) => await axios.post('/tasks', task)
